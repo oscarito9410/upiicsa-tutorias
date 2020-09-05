b256d074c7207859cb6a9ce6bc96daed1141f414
Oscar Michel Huerta Cruz <MichelHuerta@MichelHC.local>
2020-09-05 18:24:49 -0500

    Nuevo proyecto

---
+++ app/src/main/java/com/example/upicca_tutorias/ui/student_registry/RegistryStudentFragment.kt
@@ -0,0 +1,31 @@
+package com.example.upicca_tutorias
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RegistryStudentFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RegistryStudentFragment()
+    }
+
+    private lateinit var viewModel: RegistryStudentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.registry_student_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(RegistryStudentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/upicca_tutorias/ui/student_registry/RegistryStudentViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.upicca_tutorias
+
+import androidx.lifecycle.ViewModel
+
+class RegistryStudentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
