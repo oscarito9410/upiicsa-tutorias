b22bbb1ce9ea001a1f1f259088cd1d6162aea9bb
MichelHuerta <michelyogta09@gmail.com>
2020-09-05 19:19:20 -0500

    - Primer commit a develop con primera navegacion en registro de alumno

--- app/src/main/java/com/example/upicca_tutorias/ui/student_registry/RegistryStudentFragment.kt
+++ app/src/main/java/com/example/upicca_tutorias/ui/student_registry/RegistryStudentFragment.kt
@@ -1,12 +1,15 @@
 package com.example.upicca_tutorias.ui.student_registry
 
+import android.opengl.Visibility
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.RenderProcessGoneDetail
 import com.example.upicca_tutorias.R
+import kotlinx.android.synthetic.main.registry_student_fragment.*
 
 class RegistryStudentFragment : Fragment() {
 
@@ -29,5 +32,19 @@ class RegistryStudentFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(RegistryStudentViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tv_registry_footer.setText(String.format(getString(R.string.text_registry_footer,"1")))
+
+        iv_registry_footer.setOnClickListener {
+            tv_registry_footer.setText(String.format(getString(R.string.text_registry_footer,"2")))
+            cly_container_registry_step_1.visibility = View.GONE
+            cly_container_registry_step_2.visibility = View.VISIBLE
+        }
+
+
+    }
+
 
 }
