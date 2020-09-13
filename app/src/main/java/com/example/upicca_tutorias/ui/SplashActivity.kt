85a7743a575d519c5437d29eb0daccf552d16e1b
Oscar Pérez <oscar121094@gmail.com>
2020-09-13 16:14:47 -0500

    Remove white spaces

--- app/src/main/java/com/example/upicca_tutorias/ui/SplashActivity.kt
+++ app/src/main/java/com/example/upicca_tutorias/ui/SplashActivity.kt
@@ -9,6 +9,7 @@ import com.example.upicca_tutorias.R
 import com.example.upicca_tutorias.ui.signin.SiginInActivity
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
