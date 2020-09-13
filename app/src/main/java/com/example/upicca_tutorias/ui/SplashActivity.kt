5f4db06cfef25c479ae113374497ffb99c51f667
Oscar Pérez <oscar121094@gmail.com>
2020-09-13 16:11:44 -0500

    Feature splash screen was added

---
+++ app/src/main/java/com/example/upicca_tutorias/ui/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.upicca_tutorias.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.upicca_tutorias.R
+import com.example.upicca_tutorias.ui.signin.SiginInActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, SiginInActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
